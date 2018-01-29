3913f1e7041964daa31142f504f7fa5ab736563e
Tim A. Conner <timaconner1@gmail.com>
2018-01-29 13:12:11 -0600

    Add ItemFactory.

---
+++ app/factories/ItemFactory.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('ToDoApp').factory('ItemFactory', () => {
+
+    let items = [
+        {
+            id: 0,
+            task: "mow the lawn",
+            isCompleted: false,
+            dueDate: "12/5/17",
+            assignedTo: "Greg",
+            location: "Joe's house",
+            urgency: "low",
+            dependencies: "sunshine, clippers, hat, water, headphones"
+        },
+        {
+            id: 1,
+            task: "grade quizzes, I mean Mastery Watzits",
+            isCompleted: false,
+            dueDate: "12/5/17",
+            assignedTo: "Joe",
+            location: "NSS",
+            urgency: "high",
+            dependencies: "wifi, tissues, vodka"
+        },
+        {
+            id: 2,
+            task: "take a nap",
+            isCompleted: false,
+            dueDate: "5/21/18",
+            assignedTo: "Joe",
+            location: "Porch of lakefront cabin",
+            urgency: "medium",
+            dependencies: "hammock, silence"
+        }
+    ];
+
+    let getToDoItems = () => {
+        return items;
+    };
+
+    let addNewItem = (toDoItem) => {
+        toDoItem.id = items.length + 1;// Increment unique id by one
+        items.push(toDoItem);
+    };
+
+    return {getToDoItems, addNewItem};
+});
