031c8a52a78a5d878a4d952d0d75eb3d48c27f78
Tim A. Conner <timaconner1@gmail.com>
2018-01-29 13:13:47 -0600

    Add app js and all routing.

--- app/app.js
+++ app/app.js
@@ -1,34 +1,25 @@
 "use strict";
 
-// $scope.items = [
-//   {
-//     id: 0,
-//     task: "mow the lawn",
-//     isCompleted: false,
-//     dueDate: "12/5/17",
-//     assignedTo: "Greg",
-//     location: "Joe's house",
-//     urgency: "low",
-//     dependencies: "sunshine, clippers, hat, water, headphones"
-//   },
-//   {
-//     id: 1,
-//     task: "grade quizzes, I mean Mastery Watzits",
-//     isCompleted: false,
-//     dueDate: "12/5/17",
-//     assignedTo: "Joe",
-//     location: "NSS",
-//     urgency: "high",
-//     dependencies: "wifi, tissues, vodka"
-//   },
-//   {
-//     id: 2,
-//     task: "take a nap",
-//     isCompleted: false,
-//     dueDate: "5/21/18",
-//     assignedTo: "Joe",
-//     location: "Porch of lakefront cabin",
-//     urgency: "medium",
-//     dependencies: "hammock, silence"
-//   }
-// ];
+angular.module("ToDoApp", ["ngRoute"])
+.config(($routeProvider) => {
+    /*
+     Route provider will break after it finds a when that matches.  
+     If /items/:id is at the top, it will catch all and not allow /list or /new.
+    */
+    $routeProvider
+    
+    .when('/items/list', {
+        templateUrl: 'partials/item-list.html',
+        controller: 'ItemListCtrl',
+    })
+    .when('/items/new', {
+        templateUrl: 'partials/item-new.html',
+        controller: 'ItemNewCtrl'
+    })
+    .when('/items/deets/:id', {
+        templateUrl: 'partials/item-details.html',
+        controller: 'ItemDetailCtrl'
+    })
+    .otherwise('/items/list');
+});
+
